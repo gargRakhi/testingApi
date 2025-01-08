e934f74cda51daf036d1c655e937e6841d27df7b
gargRakhi <168708105+gargRakhi@users.noreply.github.com>
2025-01-08 14:46:41 +0530

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+const express  = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("landing page");
+})
+app.get("/about",(req,res)=>{
+    res.send("about page");
+})
+
+app.listen(3000);
