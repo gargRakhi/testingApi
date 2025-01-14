26ce63465cc34a19b6a1d5ecb5f261681f943469
gargRakhi <168708105+gargRakhi@users.noreply.github.com>
2025-01-14 10:52:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const cors = require('cors');
+
+app.use(cors());
+
 app.use(express.json());
 
 app.post("/login", (req, res) => {
